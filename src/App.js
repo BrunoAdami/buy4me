7371784d1e729d358f6ef4c10e8294ff9eee5f65
Matheus Mac <gomes.matheusfelipe@gmail.com>
2019-10-17 17:05:07 -0300

    colored buttons first version

--- src/App.js
+++ src/App.js
@@ -4,13 +4,34 @@ import './App.css';
 import Header from './components/molecules/header';
 import Footer from './components/molecules/footer';
 import GPaper from './components/atoms/gpaper';
+import GButton from './components/atoms/button';
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
       <Header userName="PAULA" />
-      <GPaper>Ola</GPaper>
+      <GPaper>
+        <GButton
+          text='blue'
+          type='blue'
+        /> 
+
+        <GButton
+          text='purple'
+          type='purple'
+        /> 
+
+        <GButton
+          text='orange'
+          type='orange'
+        /> 
+
+        <GButton
+          text='red'
+          type='red'
+        /> 
+      </GPaper>
       <Footer />
     </div>
   );
---
+++ src/components/atoms/button/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+export const BlueButton = styled(Button)`
+    background-color: #0E7EBD !important;
+    color: white !important;
+`;
+
+export const PurpleButton = styled(Button)`
+    background-color: #AE2996 !important;
+    color: white !important;
+`;
+
+export const OrangeButton = styled(Button)`
+    background-color: #E4670C !important;
+    color: white !important;
+`;
+
+export const RedButton = styled(Button)`
+    background-color: #E32910 !important;
+    color: white !important;
+`;
